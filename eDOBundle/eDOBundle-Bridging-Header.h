12f6ee748c5d3026e5b865a7989dfa0375144e0b
Szymon Kazmierczak <szymonkaz@gmail.com>
2019-07-06 21:04:53 +0100

    eh

---
+++ eDOBundle/eDOBundle-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "AppFramework/Action/GREYAction.h"
+#import "AppFramework/Action/GREYActionBlock.h"
+#import "AppFramework/Action/GREYActions.h"
+#import "AppFramework/Synchronization/GREYSyncAPI.h"
+#import "CommonLib/DistantObject/GREYHostApplicationDistantObject.h"
+#import "CommonLib/Matcher/GREYElementMatcherBlock.h"
+#import "CommonLib/Matcher/GREYMatcher.h"
